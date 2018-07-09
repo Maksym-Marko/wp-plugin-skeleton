84cb4da6575b33fd764fbc737818839277cdab4b
Maksym Marko <markomaksym@gmail.com>
2018-07-09 13:03:01 +0300

    Bug enqueue style

--- includes/frontend/class-frontend-main.php
+++ includes/frontend/class-frontend-main.php
@@ -21,7 +21,7 @@ class |UNIQUESTRING|FrontEndMain
 
 			wp_enqueue_style( '|uniquestring|_font_awesome', |UNIQUESTRING|_PLUGIN_URL . 'assets/font-awesome-4.6.3/css/font-awesome.min.css' );
 
-			wp_enqueue_style( '|uniquestring|_style', |UNIQUESTRING|_PLUGIN_URL . 'includes/frontend/assets/css/style.css', array( '|UNIQUESTRING|_font_awesome' ), |UNIQUESTRING|_PLUGIN_VERSION, 'all' );
+			wp_enqueue_style( '|uniquestring|_style', |UNIQUESTRING|_PLUGIN_URL . 'includes/frontend/assets/css/style.css', array( '|uniquestring|_font_awesome' ), |UNIQUESTRING|_PLUGIN_VERSION, 'all' );
 
 			wp_enqueue_script( '|uniquestring|_script', |UNIQUESTRING|_PLUGIN_URL . 'includes/frontend/assets/js/script.js', array( 'jquery' ), |UNIQUESTRING|_PLUGIN_VERSION, false );
 
